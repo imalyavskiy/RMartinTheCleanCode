a42b1bd048c6ac6cafb498940073f6bf0d408a93
Ilya Malyavskiy <ilya.malyavskiy@gmail.com>
2022-05-16 10:45:31 +0700

    String arguments parsing added.

--- ArgParserDirty/src/ArgParserDirty/Application.java
+++ ArgParserDirty/src/ArgParserDirty/Application.java
@@ -1,10 +1,39 @@
 package ArgParserDirty;
 
+import java.text.ParseException;
+
 // "l,p#,d*" == "-l -p 65536 -d letsplay"
 
 public class Application {
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		Args arg = new Args("l,p#,d*", args);
+		try 
+		{
+			Args arg = new Args("l,p#,d*", args);
+			if(!arg.isValid())
+			{
+				throw new Exception("Parsing failed.");
+			}
+			
+			boolean logging = arg.getBoolean('l');
+			String directory = arg.getString('d');
+			
+			Application app = new Application();
+			app.execute(logging, directory);
+		} 
+		catch (ParseException e) 
+		{
+			System.out.printf("Argument error: %s\n", e.getMessage());
+		}
+		catch (Exception e)
+		{
+			System.out.printf(e.getMessage());
+		}
+		
+	}
+	
+	private void execute(boolean logging, String directory)
+	{
+		System.out.println(String.format("execute: logging=%b, directory=%s", logging, directory));
 	}
 }
--- ArgParserDirty/src/ArgParserDirty/Args.java
+++ ArgParserDirty/src/ArgParserDirty/Args.java
@@ -1,30 +1,33 @@
 package ArgParserDirty;
 
+import java.text.ParseException;
 import java.util.*;
 
 public class Args 
 {
 	private String schema;
 	private String[] args;
-	private boolean valid;
+	private boolean valid = true;
 	private Set<Character> unexpectedArguments = new TreeSet<Character>();
 	private Map<Character, Boolean> booleanArgs = new HashMap<Character, Boolean>();
-	private int numberOfArguments = 0;
+	private Map<Character, String> stringArgs = new HashMap<Character, String>();
+	private Set<Character> argsFound = new HashSet<Character>();
+	private int currentArgument;
+	private char errorArgument = '\0';
+	
+	enum ErrorCode {OK, MISSING_STRING};
+	
+	private ErrorCode errorCode = ErrorCode.OK;
 
-	public Args(String schema, String[] args) 
+	public Args(String schema, String[] args) throws ParseException
 	{
 		this.schema = schema;
 		this.args = args;
 		
 		valid = parse();
 	}
-	
-	public boolean isValid() 
-	{
-		return valid;
-	}
-	
-	private boolean parse() 
+
+	private boolean parse() throws ParseException 
 	{
 		if (schema.length() == 0 && args.length == 0)
 		{
@@ -34,40 +37,71 @@ public class Args
 		parseSchema();
 		parseArguments();
 		
-		return unexpectedArguments.size() == 0;
+		return valid;
 	}
 	
-	private boolean parseSchema() 
+	private boolean parseSchema() throws ParseException 
 	{
 		for (String element : schema.split(",")) 
 		{
-			parseSchemaElement(element);
+			if(element.length() > 0)
+			{
+				String trimmedElement = element.trim();
+				parseSchemaElement(trimmedElement);
+			}
 		}
 		return true;
 	}
 	
-	private void parseSchemaElement(String element) 
+	private void parseSchemaElement(String element) throws ParseException 
 	{
-		if (element.length() == 1) 
+		char elementId = element.charAt(0);
+		String elementTail = element.substring(1);
+		validateSchemaElementId(elementId);
+		
+		if (isBooleanSchemaElement(elementTail))
 		{
-			parseBooleanSchemaElement(element);
+			parseBooleanSchemaElement(elementId);
+		}
+		else if (isStringSchemaElement(elementTail))
+		{
+			parseStringSchemaElement(elementId);
 		}
 	}
 	
-	private void parseBooleanSchemaElement(String element) 
+	private void validateSchemaElementId(char elementId) throws ParseException 
 	{
-		char c = element.charAt(0);
-		
-		if (Character.isLetter(c)) 
+		if (!Character.isLetter(elementId)) 
 		{
-			booleanArgs.put(c, false);
+			throw new ParseException("Bad character:" + elementId + "in Args format: " + schema, 0);
 		}
+	}
+	
+	private void parseStringSchemaElement(char elementId) 
+	{
+		stringArgs.put(elementId, "");
+	}
+	
+	private boolean isStringSchemaElement(String elementTail) 
+	{
+		return elementTail.equals("*");
+	}
+	
+	private boolean isBooleanSchemaElement(String elementTail) 
+	{
+		return elementTail.length() == 0;
+	}	
+	
+	private void parseBooleanSchemaElement(char elementId) 
+	{
+		booleanArgs.put(elementId, false);
 	}
 	
 	private boolean parseArguments() 
 	{
-		for (String arg : args)
+		for (currentArgument = 0; currentArgument < args.length; currentArgument++)
 		{
+			String arg = args[currentArgument];
 			parseArgument(arg);
 		}
 		
@@ -91,17 +125,57 @@ public class Args
 	
 	private void parseElement(char argChar) 
 	{
-		if (isBoolean(argChar)) 
+		if (setArgument(argChar))
 		{
-			numberOfArguments++;
-			setBooleanArg(argChar, true);
+			argsFound.add(argChar);
 		}
 		else 
 		{
 			unexpectedArguments.add(argChar);
+			valid = false;
 		}
 	}
 	
+	private boolean setArgument(char argChar) 
+	{
+		boolean set = true;
+		
+		if (isBoolean(argChar))
+		{
+			setBooleanArg(argChar, true);
+		}
+		else if (isString(argChar))
+		{
+			setStringArg(argChar, "");
+		}
+		else
+		{
+			set = false;
+		}
+		
+		return set;
+	}
+	
+	private void setStringArg(char argChar, String s) 
+	{
+		currentArgument++;
+		try 
+		{
+			stringArgs.put(argChar, args[currentArgument]);
+		} 
+		catch (ArrayIndexOutOfBoundsException e) 
+		{
+			valid = false;
+			errorArgument = argChar;
+			errorCode = ErrorCode.MISSING_STRING;
+		}
+	}
+	
+	private boolean isString(char argChar) 
+	{
+		return stringArgs.containsKey(argChar);
+	}
+
 	private void setBooleanArg(char argChar, boolean value) 
 	{
 		booleanArgs.put(argChar, value);
@@ -114,24 +188,38 @@ public class Args
 	
 	public int cardinality() 
 	{
-		return numberOfArguments;
+		return argsFound.size();
 	}
 	
 	public String usage() 
 	{
 		if (schema.length() > 0)
-		return "-["+schema+"]";
+		{
+			return "-["+schema+"]";
+		}
 		else
-		return "";
+		{
+			return "";
+		}
 	}
 	
-	public String errorMessage() 
+	public String errorMessage() throws Exception 
 	{
 		if (unexpectedArguments.size() > 0) 
 		{
 			return unexpectedArgumentMessage();
+		} 
+		else
+		{
+			switch (errorCode) 
+			{
+				case MISSING_STRING:
+					return String.format("Could not find string parameter for -%c.", errorArgument);
+				case OK:
+					throw new Exception("TILT: Should not get here.");
+			}
 		}
-		
+
 		return "";
 	}
 	
@@ -151,5 +239,29 @@ public class Args
 	public boolean getBoolean(char arg) 
 	{
 		return booleanArgs.get(arg);
+	}
+	
+	private boolean falseIfNull(Boolean b) 
+	{
+		return b == null ? false : b;
+	}
+	
+	public String getString(char arg) 
+	{
+		return blankIfNull(stringArgs.get(arg));
+	}
+	
+	private String blankIfNull(String s) 
+	{
+		return s == null ? "" : s;
+	}
+	
+	public boolean has(char arg) 
+	{
+		return argsFound.contains(arg);
+	}
+	
+	public boolean isValid() {
+		return valid;
 	}
 }
