02818ca51e36920f3809a261c8b315e395e261fa
Ilya Malyavskiy <ilya.malyavskiy@gmail.com>
2022-05-16 22:09:17 +0700

    Draft argument marshallers added. Boolean marshaller had been put in its place.

--- ArgParserDirty/src/ArgParserDirty/Args.java
+++ ArgParserDirty/src/ArgParserDirty/Args.java
@@ -9,7 +9,7 @@ public class Args
 	private String[] args;
 	private boolean valid = true;
 	private Set<Character> unexpectedArguments = new TreeSet<Character>();
-	private Map<Character, Boolean> booleanArgs = new HashMap<Character, Boolean>();
+	private Map<Character, BooleanArgumentMarshaler> booleanArgs = new HashMap<Character, BooleanArgumentMarshaler>();
 	private Map<Character, String> stringArgs = new HashMap<Character, String>();
 	private Map<Character, Integer> intArgs = new HashMap<Character, Integer>();
 	private Set<Character> argsFound = new HashSet<Character>();
@@ -96,7 +96,7 @@ public class Args
 	
 	private void parseBooleanSchemaElement(char elementId) 
 	{
-		booleanArgs.put(elementId, false);
+		booleanArgs.put(elementId, new BooleanArgumentMarshaler());
 	}
 	
 	private void parseIntegerSchemaElement(char elementId) 
@@ -242,7 +242,7 @@ public class Args
 
 	private void setBooleanArg(char argChar, boolean value) 
 	{
-		booleanArgs.put(argChar, value);
+		booleanArgs.get(argChar).setBoolean(value);
 	}
 	
 	private boolean isBooleanArg(char argChar) 
@@ -325,7 +325,7 @@ public class Args
 		
 	public boolean getBoolean(char arg) 
 	{
-		return booleanArgs.get(arg);
+		return falseIfNull(booleanArgs.get(arg).getBoolean());
 	}
 	
 	public boolean has(char arg) 
@@ -339,6 +339,33 @@ public class Args
 	}
 	
 	private class ArgsException extends Exception 
+	{
+	}
+	
+	private class ArgumentMarshaler 
+	{
+		private boolean booleanValue = false;
+		
+		public void setBoolean(boolean value) 
+		{
+			booleanValue = value;
+		}
+		
+		public boolean getBoolean() 
+		{
+			return booleanValue;
+		}
+	}
+	
+	private class BooleanArgumentMarshaler extends ArgumentMarshaler 
+	{
+	}
+	
+	private class StringArgumentMarshaler extends ArgumentMarshaler 
+	{
+	}
+	
+	private class IntegerArgumentMarshaler extends ArgumentMarshaler 
 	{
 	}
 }
