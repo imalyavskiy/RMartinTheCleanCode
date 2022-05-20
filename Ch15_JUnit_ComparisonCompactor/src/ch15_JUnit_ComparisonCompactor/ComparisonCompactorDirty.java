af630ad674e68af9418badbdd75f4573b9054b15
Ilya Malyavskiy <ilya.malyavskiy@gmail.com>
2022-05-20 13:00:55 +0700

    [N4] Name ambiguity eliminated.

--- Ch15_JUnit_ComparisonCompactor/src/ch15_JUnit_ComparisonCompactor/ComparisonCompactorDirty.java
+++ Ch15_JUnit_ComparisonCompactor/src/ch15_JUnit_ComparisonCompactor/ComparisonCompactorDirty.java
@@ -31,9 +31,9 @@ public class ComparisonCompactorDirty
 		
 		findCommonPrefix();
 		findCommonSuffix();
-		String expected = compactString(this.expected);
-		String actual = compactString(this.actual);
-		return Assert.format(message, expected, actual);
+		String compactExpected = compactString(expected);
+		String compactActual = compactString(actual);
+		return Assert.format(message, compactExpected, compactActual);
 	}
 	
 	private boolean shouldNotCompact() {
